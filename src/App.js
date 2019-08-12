4bc65dfdfddbc8015eef4873674ce8ed3ed8558a
Scott Renz <scottrenz@scottrenz.com>
2019-08-12 19:56:01 -0400

    make removeItem be consumed from useContext

--- src/App.js
+++ src/App.js
@@ -18,36 +18,20 @@ function App() {
 	};
 	const removeItem = item => {
    const newCart = []
-
 		for (let i=0;i<cart.length;i++)
 {
-		// console.log('apps cart',item,i ,cart[i])
 if( parseInt( cart[i]['id']) !== parseInt( item))
 {
 	newCart.push(cart[i])
 }
 	}
 setCart(newCart)
-for (let i=0;i<cart.length;i++)
-{
-		// console.log('new apps cart',item,i ,cart[i])
-}
-for (let i=0;i<newCart.length;i++)
-{
-		// console.log('newCart apps cart',item,i ,newCart[i])
-}
-// const newItem = item.map((_, index) => index !== item.id);
-
-		// setCart([...cart, newItem]);
-
-		// const newTodos = todos.filter((_, index) => index !== todoIndex);
-
 	};
 
 	return (
 		<div className="App">
-	<ProductContext.Provider value={{ products, addItem, removeItem }}>
-	<CartContext.Provider value={{ cart, removeItem }}>
+	<ProductContext.Provider value={{ products, addItem }}>
+	<CartContext.Provider value={{ cart, removeItem, products }}>
 			<Navigation cart={cart} />
 
 			{/* Routes */}
--- src/components/ShoppingCartItem.js
+++ src/components/ShoppingCartItem.js
@@ -1,6 +1,9 @@
-import React from 'react';
+import React, {useContext} from 'react';
+import {CartContext} from '../contexts/CartContext'
 
 const Item = props => {
+	const { cart, removeItem, item, products } = useContext(CartContext);
+
 	console.log('item props',props)
 	return (
 		<div className="shopping-cart_item">
@@ -10,7 +13,7 @@ const Item = props => {
 			<div>
 				<h1>{props.title}</h1>
 				<p>$ {props.price}</p>
-				<button onClick={() => props.removeItem(props.id)}>
+				<button onClick={() => removeItem(props.id)}>
 				Remove from cart
 			</button>
 
